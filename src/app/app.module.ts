b8217ac40682f42b7602ec4e7a2ffbedb3f88f15
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-02-20 15:16:37 +0200

    First assignment (Alert & Success)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './warning-alert/warning-alert.component';
+import { SuccessalertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningalertComponent,
+    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
